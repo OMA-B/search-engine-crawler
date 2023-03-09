78f031a847a342e296552dae96f653a2f0fa1934
Daniel Obiorah <Beetroit3266@gmail.com>
2023-03-09 12:17:40 +0100

    uncomment and test the codes

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
 import requests
 url='http://127.0.0.1:5000'
-data = {'username':'Fathead','password':'Pajhabsi8','email':'ansk@askjil.corn'}
 headers ={'Content-Type':'application/json'}
-r = requests.post(url+'/signup',headers=headers,json=data)
-print(r.text)
+# data = {'username':'jbiidjhs','password':'fgddfgd','email':'ijbh@adsjdbi.corn'}
+# r = requests.post(url+'/signup',headers=headers,json=data)
+# print(r.text)
+# data = {'username':'Fosead','password':'ksajdnsi8','email':'ajahbsk@askjil.corn'}
+# r = requests.post(url+'/signup',headers=headers,json=data)
+# print(r.text)
+# data = {'username':'jhbjhbi','password':'dnkscjsjdj','email':'kajska@askjil.corn'}
+# r = requests.post(url+'/signup',headers=headers,json=data)
+# print(r.text)
 
+# test='user1@gmail.yam'
+# del data['username']
+# r=requests.post(url+'/login',json=data,headers=headers)
+# print(r.text)
 
-del data['username']
-r=requests.post(url+'/login',json=data,headers=headers)
-print(r.text)
+# r=requests.get(url+'/users')
+# print(r.text)
+# # r=requests.post(url+'/delete',json={'email':test},headers=headers)
+# print(r.text)
+
+data = {'search_engine_name':'google','search_phrase':'Yam and beans','page_depth_num':10,'max_search_num':10}
+r = requests.post(url+'/scrape',headers=headers,json=data)
