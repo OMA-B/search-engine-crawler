0bfa0346a323c9ce1dd96ff3ac692fa4627df949
Michael Adeshina <michaelking4christ@gmail.com>
2023-03-17 19:04:46 +0100

    mistake corrected

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from crawler import run_crawler
 
 app = Flask('Search Engine Crawler')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://realstem_crawler:stemray$$$###@localhost/realstem_search_engine'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://realstem_crawler:password@localhost/db_name'
 CORS(app)
 db = SQLAlchemy(app)
 
